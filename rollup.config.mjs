5918707347ce3b16b347f2941baf3110c0410433
jeffrey xu <jianfeng.xu@cssca.com>
2025-04-28 14:39:31 +0800

    chore: UPDATE

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
+import copy from 'rollup-plugin-copy'; // 导入插件
 
 export default {
     input: 'src/index.ts',
@@ -21,13 +22,13 @@ export default {
         {
             file: 'dist/index.umd.js',  // 从 dist/umd/ 移动到 dist/
             format: 'umd',
-            name: 'CssType',
+            name: 'Signals',
             sourcemap: true
         },
         {
             file: 'dist/index.min.js',
             format: 'umd',
-            name: 'CssType',
+            name: 'Signals',
             plugins: [terser()],
             sourcemap: true
         }
@@ -35,12 +36,17 @@ export default {
     plugins: [
         nodeResolve(),
         commonjs(),
-        typescript({ tsconfig: './tsconfig.json' }),  // 显式指定 tsconfig 路径
+        typescript({tsconfig: './tsconfig.json'}),  // 显式指定 tsconfig 路径
         babel({
             babelHelpers: 'bundled',
             extensions: ['.ts'],
-            presets: [['@babel/preset-env', { targets: '> 0.25%, not dead' }]]
-        })
+            presets: [['@babel/preset-env', {targets: '> 0.25%, not dead'}]]
+        }),
+        copy({ // 新增配置
+            targets: [
+                {src: ['package.json', 'README.md'], dest: 'dist/'}, // 复制到 dist/ 目录
+            ],
+        }),
     ],
     external: id => /node_modules/.test(id) // 修正条件：仅排除 node_modules
 }
